7383d07e6c3ae71457e683046c7cf68b218f6ce4
Alexey Borzykh <alexey.borzykh@my.games>
2025-09-05 21:31:49 +0300

    v2.2.1: Обновление документации по версионированию и исключение reports из Git

--- tmp/gitUtils/git_workflow.bat
+++ tmp/gitUtils/git_workflow.bat
@@ -18,7 +18,23 @@ git add .
 echo.
 
 echo 4. Committing changes...
-git commit -m "v2.2.0: Automated commit - %date% %time%"
+echo    Choose version type:
+echo    [1] Patch (X.Y.Z+1) - bug fixes, small changes
+echo    [2] Minor (X.Y+1.0) - new features, backward compatible
+echo    [3] Major (X+1.0.0) - breaking changes, architecture changes
+echo.
+set /p version_type="Enter version type (1-3): "
+
+if "%version_type%"=="1" (
+    git commit -m "v2.2.1: Patch update - %date% %time%"
+) else if "%version_type%"=="2" (
+    git commit -m "v2.3.0: Minor update - %date% %time%"
+) else if "%version_type%"=="3" (
+    git commit -m "v3.0.0: Major update - %date% %time%"
+) else (
+    echo Invalid choice, using patch version...
+    git commit -m "v2.2.1: Patch update - %date% %time%"
+)
 echo.
 
 echo 5. Pushing to repository...
