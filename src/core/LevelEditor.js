76aec3f85d7d686355e2a1b6485bae783e667e05
Alexey Borzykh <alexey.borzykh@my.games>
2025-09-22 18:47:36 +0300

    v3.9.0: Update version after OutlinerPanel improvements

--- src/core/LevelEditor.js
+++ src/core/LevelEditor.js
@@ -36,7 +36,7 @@ export class LevelEditor {
      * @static
      * @type {string}
      */
-    static VERSION = '3.8.7';
+    static VERSION = '3.9.0';
 
     constructor(userPreferencesManager = null) {
         // Initialize managers
